83a5a8bc0977ef6f5569c271e232c93bc2334484
Panathes <dianketohead@gmail.com>
2020-04-28 22:27:59 +0200

    fix path issue

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path')
 
-let connection = new sqlite3.Database("../db/portfolio", sqlite3.OPEN_READWRITE, (err) => {
+const dbPath = path.resolve(__dirname, 'portfolio.db')
+// const db = new sqlite3.Database(dbPath)
+
+let connection = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       console.error(err.message);
     }
